1a570e92a6e413cb155256db21213645eaadd075
nquizx <imdangodaane@gmail.com>
2019-11-04 16:15:00 +0700

    update styles.scss (add icielAvantGardeGothic font face

--- src/app/shared/_components/navbar/navbar.component.ts
+++ src/app/shared/_components/navbar/navbar.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  headers = [
+    { name: 'Trang chủ', url: 'home'},
+    { name: 'Tải game', url: 'download'},
+    { name: 'Thông tin', url: 'information'},
+    { name: 'Hỗ trợ', url: 'support'},
+    { name: 'Tin tức', url: 'news'},
+    { name: 'Diễn đàn', url: 'forums'},
+    { name: 'Wiki', url: 'wiki'},
+    { name: 'Đăng ký', url: 'register'},
+  ]
 
   constructor() { }
 
